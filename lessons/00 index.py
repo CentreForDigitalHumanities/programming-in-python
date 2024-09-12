ddcbda7d5bba452616148c4bb5ea7023a27377ec
Arjan Mossel <arjanmossel@gmail.com>
2024-09-12 16:01:22 +0200

    Convert to jupytext paired notebooks
    lessons/*.ipynb notebooks do not use cell outputs and so do not need
    to be kept under version control.

---
+++ lessons/00 index.py
@@ -0,0 +1,55 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3
+#     name: python3
+# ---
+
+# %% [markdown] id="RQ73d6XVysi7"
+# # CDH course "Programming in Python"
+#
+# **April 2023 edition**
+#
+# [Teams channel](https://teams.microsoft.com/l/channel/19%3aAlr7pcE2v9p1eaKT1BtNxJAB9mgCFT1olZvAMswJlEc1%40thread.tacv2/Algemeen?groupId=7fd15384-6748-4852-8646-d96963fb3524&tenantId=d72758a0-a446-4e0f-a0aa-4bf95a4a10e7)
+#
+# ## Course modules
+#
+# 1. [Introduction](https://colab.research.google.com/drive/1i4wJpUIr77Fh1renWNjt00Bd51NbC1nB)
+# 2. [Values and expressions](https://colab.research.google.com/drive/17K6C_EZoeGtRxoTbYQvygFEdpWgQ2QFp)
+# 3. [Conditionals](https://colab.research.google.com/drive/1KkZ2fS75o7giccQJakRhyvYBV7JdFnVw)
+# 4. [Datastructures](https://colab.research.google.com/drive/1JxzmIzwcipnwFBntv0WZOlT-d2fUjoRF)
+# 5. [Assertions](https://colab.research.google.com/drive/1Nv6vgkH2zjJes7LXUCVhsDwFOGCtTbHM)
+# 6. [Loops](https://colab.research.google.com/drive/1AZY4ESmsKKMvbalBDLlMrezAM5NzXXxV)
+# 7. [Functions](https://colab.research.google.com/drive/1APhegD_KpLRFn5bC6Ater2wFpT8_Opfr) 
+# 8. [Debugging](https://colab.research.google.com/drive/1yQskT6SyKvXtXewx5kCla2NOmasgP8Vi)
+# 9. [String manipulation](https://colab.research.google.com/drive/1Z7NNMxqHTSMoUadH3pVL6Bg6oii4qUoQ)
+# 10. [Dictionaries](https://colab.research.google.com/drive/16mecEPkVGUBIFoHIALO7dE8qv7PynHL9)
+# 11. [Working with files](https://colab.research.google.com/drive/17G-gA5-GNKX-mQglrPVNtndV2YhBafBr)
+#
+# ## Extra material
+#
+# - [Functions exercises](https://colab.research.google.com/drive/13__hh18xcBeZ5xjeby21KXUBYjRpb_Vy)
+# - [Tips](https://colab.research.google.com/drive/1qKVaI3Ct1hOsM_06mZdXpx0sLSR_O-6R)
+#
+# ## Projects
+# - [Text analysis](https://colab.research.google.com/drive/1wUgaVE70dzjIlIEuZtgQalqYI2UcfiXK)
+#
+# ## Exercise solutions
+#
+# 1. [Introduction](https://colab.research.google.com/drive/1RIwG7hA-Ymjcm1dmSW6wA_hu1clDNrAd)
+# 2. [Values and expressions](https://colab.research.google.com/drive/1JKeNehBZ9hhHXPdVunQ_r9cv-lLR9Byy)
+# 3. [Conditionals](https://colab.research.google.com/drive/1Nvvjc3fGnMg2tWvfw2W1gVn2oKKbWzGI)
+# 4. [Datastructures](https://colab.research.google.com/drive/1juOQFMlRmeVUfbKWy0wtZ9lWkSZmr2Gn)
+# 5. [Assertions](https://colab.research.google.com/drive/1tqjXsHnnJeEAivrxqRoOSW9eNESCwLIK)
+# 6. [Loops](https://colab.research.google.com/drive/1vy2gKHtBUMk60u2bmCYlcqmhIFpI2PI-)
+# 7. [Functions](https://colab.research.google.com/drive/13M4t34msJvudQTN-Im24w4iRfctOuNCv)
+# 8. Debugging - no solutions
+# 9. [String manipulation](https://colab.research.google.com/drive/1Z7NNMxqHTSMoUadH3pVL6Bg6oii4qUoQ)
+# 10. [Dictionaries](https://colab.research.google.com/drive/1Qw7vgiyRVg1E-BkUEvCxX0e6QZQ9y_k6)
+# 11. [Working with files](https://colab.research.google.com/drive/1Nz3lhsntgw4yAI6257drrp-EtEQEVogF)
